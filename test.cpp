d01de6b3380d8bde4f6c1ff78d555ee7ad437c3e
신승겸 <sk2003kr@naver.com>
2025-10-11 18:00:30 +0900

    초기 상태

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    cout << n;
+int main() 
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    
+
     return 0;
 }

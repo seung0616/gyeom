3ca7ba8b7da0067f42f9958e0861fa474ba310e6
신승겸 <sk2003kr@naver.com>
2025-10-11 21:39:34 +0900

    백준 10998번 - AxB

--- Solutions/10998.cpp
+++ Solutions/10998.cpp
@@ -6,12 +6,9 @@ int main()
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    double A, B;
+    int A, B;
     cin >> A >> B;
-    
-    cout.precision(10);
-    cout << fixed;
-    cout << A / B;
+    cout << A * B;
 
     return 0;
 }

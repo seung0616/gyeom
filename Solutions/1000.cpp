8716f379c30e864c8c4662af29d868a93e633a4e
신승겸 <sk2003kr@naver.com>
2025-10-11 21:07:22 +0900

    백준 1000번

---
+++ Solutions/1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+
+    return 0;
+}

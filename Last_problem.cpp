337d110e104a0048f67170267eeb2b1d318205dc
신승겸 <sk2003kr@naver.com>
2025-10-23 15:00:07 +0900

    백준 10814번 - 나이순 정렬

--- Last_problem.cpp
+++ Last_problem.cpp
@@ -1,3 +1,7 @@
+/*정렬은 안정 정렬과 불안정 정렬이 있다. 
+  안정정렬은 조건이 같을 때 입력 순서가 유지되는 정렬이고 불안정정렬은 그렇지 않은 정렬이다.
+  sort함수는 불안정정렬이기 때문에 안정정렬인 stable_sort를 사용해야한다.
+  또는 person 구조체에 인덱스를 추가해서 해결 할 수 있다.*/
 #include <iostream>
 #include <vector>
 #include <string>
@@ -6,15 +10,13 @@ using namespace std;
 
 struct person
 {
-  int num;
   int age;
   string name;
 };
 
 bool compare(person a, person b)
 {
-  if(a.age == b.age) return a.num < b.num;
-  else return a.age < b.age;
+  return a.age < b.age;
 }
 
 int main() 
@@ -29,11 +31,10 @@ int main()
 
   for(int i = 0; i < N; i++)
   {
-    members[i].num = i;
     cin >> members[i].age >> members[i].name;
   }
 
-  sort(members.begin(), members.end(), compare);
+  stable_sort(members.begin(), members.end(), compare);
 
   for(person member : members)
   {
--- Solutions/10814.cpp
+++ Solutions/10814.cpp
@@ -6,15 +6,13 @@ using namespace std;
 
 struct person
 {
-  int num;
   int age;
   string name;
 };
 
 bool compare(person a, person b)
 {
-  if(a.age == b.age) return a.num < b.num;
-  else return a.age < b.age;
+  return a.age < b.age;
 }
 
 int main() 
@@ -29,11 +27,10 @@ int main()
 
   for(int i = 0; i < N; i++)
   {
-    members[i].num = i;
     cin >> members[i].age >> members[i].name;
   }
 
-  sort(members.begin(), members.end(), compare);
+  stable_sort(members.begin(), members.end(), compare);
 
   for(person member : members)
   {

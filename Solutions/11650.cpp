7653e8eab05a6452aa8ead3d479984dac0d6974e
신승겸 <sk2003kr@naver.com>
2025-10-23 14:31:20 +0900

    백준 11650번 - 좌표 정렬하기

---
+++ Solutions/11650.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Node
+{
+  int x, y;
+};
+
+bool compare(Node a, Node b)
+{
+  if(a.x == b.x) return a.y < b.y;
+  else return a.x < b.x;
+}
+
+int main() 
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int N;
+  cin >> N;
+
+  vector<Node> nodes(N);
+
+  for(int i = 0; i < N; i++)
+  {
+    cin >> nodes[i].x >> nodes[i].y;
+  }
+
+  sort(nodes.begin(), nodes.end(), compare);
+
+  for(int i = 0; i < N; i++)
+  {
+    cout << nodes[i].x << ' ' << nodes[i].y << '\n';
+  }
+
+  return 0;
+}
